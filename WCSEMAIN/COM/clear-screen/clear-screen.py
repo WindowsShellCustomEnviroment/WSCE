06f8ee5b6ffea94db9e9cde0c03c91d08748e15c
WindowsShellCustomEnviroment <koafarm391@gmail.com>
2024-08-25 23:13:34 +0100

    Delete WCSEMAIN directory (To replace WCSEMAIN)

--- WCSEMAIN/COM/clear-screen/clear-screen.py
+++
@@ -1,9 +0,0 @@
-import os
-import platform
-
-current_os = platform.system()
-
-if current_os == "Windows":
-	os.system("cls")
-elif current_os == "Linux" or current_os == "Darwin":
-	os.system("clear")
