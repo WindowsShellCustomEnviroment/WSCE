5fea8cfb9241b802f31e27ca2baadd7de5ca6bb6
WindowsShellCustomEnviroment <koafarm391@gmail.com>
2024-09-03 23:02:19 +0100

    Update WSCE Main System

---
+++ WSCE/WCSEMAIN/COM/hello-world/hello-world.py
@@ -0,0 +1,6 @@
+import colorama
+from colorama import Fore, Style, init
+ORANGE = "\033[38;5;208m"
+VIOLET = "\033[38;5;177m"
+INDIGO = "\033[38;5;93m"
+print(f"{Style.BRIGHT}{Fore.RED}H{ORANGE}e{Fore.YELLOW}l{Fore.GREEN}l{Fore.BLUE}o {INDIGO}W{VIOLET}o{Fore.RED}r{ORANGE}l{Fore.YELLOW}d {Fore.GREEN}F{Fore.BLUE}r{INDIGO}o{VIOLET}m {Fore.RED}t{ORANGE}h{Fore.YELLOW}e {Fore.GREEN}h{Fore.BLUE}e{INDIGO}l{VIOLET}l{Fore.RED}o{ORANGE}w{Fore.YELLOW}o{Fore.GREEN}r{Fore.BLUE}l{INDIGO}d {VIOLET}C{Fore.RED}o{ORANGE}m{Fore.YELLOW}m{Fore.GREEN}a{Fore.BLUE}n{INDIGO}d{VIOLET}!{Style.RESET_ALL}")
